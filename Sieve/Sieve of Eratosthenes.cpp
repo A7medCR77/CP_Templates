8c21dbd3c7246b2cfd5776fc8d9479ccfb6c92e4
Ayman_Ayman <107052658+A7medCR77@users.noreply.github.com>
2023-08-01 19:47:14 +0300

    Create Sieve of Eratosthenes.cpp

---
+++ Sieve/Sieve of Eratosthenes.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(s) s.size()
+#define nl '\n'
+using namespace std;
+template <typename T = int>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (auto &x : v)
+        in >> x;
+    return in;
+}
+
+template <typename T = int>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+    for (const T &x : v)
+        out << x << " ";
+    return out;
+}
+
+// time complexity n log(log(n))
+
+template <typename T = int>
+struct sieve
+{
+    vector<bool> is_prime;
+    vector<T> primes;
+    int n;
+    sieve(int n)
+    {
+        this->n = n;
+        is_prime.assign(n + 5, 1);
+
+        is_prime[0] = is_prime[1] = false;
+
+        for (int i = 2; i * i <= n; ++i)
+        {
+            if (is_prime[i])
+            {
+                for (int l = i * i; l <= n; l += i)
+                {
+                    is_prime[l] = false;
+                }
+            }
+        }
+    }
+
+    vector<T> get_primes()
+    {
+        for (int i = 1; i <= n; ++i)
+            if (is_prime[i])
+                primes.push_back(i);
+
+        return primes;
+    }
+};
+
+void solve()
+{
+    sieve<int> a(200);
+    cout << a.get_primes();
+}
+
+int main()
+{
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
